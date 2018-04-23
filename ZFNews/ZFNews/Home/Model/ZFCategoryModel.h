30bee79fd2eb0d141577e40a156423517b26498b
yanzifan-mini <yanzifan-mini@zhuyankun.local>
2018-04-23 17:46:12 +0800

    2.AF和YY的加入

---
+++ ZFNews/ZFNews/Home/Model/ZFCategoryModel.h
@@ -0,0 +1,13 @@
+//
+//  ZFCategoryModel.h
+//  ZFNews
+//
+//  Created by yanzifan-mini on 2018/4/23.
+//  Copyright © 2018年 yanzifan-mini. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ZFCategoryModel : NSObject
+
+@end
---
+++ ZFNews/ZFNews/Network/ZFBaseNetwork.m
@@ -0,0 +1,13 @@
+//
+//  ZFBaseNetwork.m
+//  ZFNews
+//
+//  Created by yanzifan-mini on 2018/4/23.
+//  Copyright © 2018年 yanzifan-mini. All rights reserved.
+//
+
+#import "ZFBaseNetwork.h"
+
+@implementation ZFBaseNetwork
+
+@end
